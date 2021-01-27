579c7c9280420b0640079d093600503adbc79f70
Lynn Tang <tanglynn2@gmail.com>
2021-01-27 12:32:48 -0600

    connecting server to database

---
+++ credentials.js
@@ -0,0 +1,7 @@
+// adding db connection as a constant
+module.exports = Object.freeze({
+  userName: 'DATABASE_USERNAME',
+  password: 'DATABASE_PASSWORD',
+  hostName: 'Database_HostName',
+  databaseName: 'DATABASE_NAME',
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+// credentials to connect to db
+const credentials = require('./credentials');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto-js');
+const app = express();
+const PORT = 8085;
+
+const Sequelize = require('sequelize');
+
+//initial sequelize to connect to db
+
+const databseConnection = new Sequelize(
+  credentials.databaseName,
+  credentials.userName,
+  credentials.password,
+  {
+    dialect: 'postgres',
+    host: credentials.hostName,
+    port: 1433, //db default port
+    logging: false,
+    dialectOptions: {
+      requestTimeout: 30000,
+    },
+  }
+);
+
+app.listen(PORT);
+console.log('server is running on http://127.0.0.1' + PORT);
