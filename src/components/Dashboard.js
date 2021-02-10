6d648c35f03e4043ab2f81a285279c5a4380ab56
Lynn Tang <tanglynn2@gmail.com>
2021-02-10 08:33:59 -0600

    set up login and registration page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Dashboard = (props) => {
   return (
     <div>
-      <h1>DASHBOARD</h1>
-      <h1>Status: {props.loggedInStatus}</h1>
+      <div>
+        <h1>Dashboard</h1>
+        <h1>Status: {props.loggedInStatus}</h1>
+      </div>
     </div>
   );
 };
